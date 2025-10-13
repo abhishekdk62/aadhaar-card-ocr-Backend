5371595eb0f20ac1bbbfca2e22ecbdcfc9ac28f8
ABHISHEK <abhidk62@gmail.com>
2025-10-14 02:55:31 +0530

    increase timeout to 5 min

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const ocrRoutes = require('./routes/ocr');
-
 const app = express();
-
-
-
 app.use(cors());
 app.use(express.json());
 app.use('/api/ocr', ocrRoutes);
